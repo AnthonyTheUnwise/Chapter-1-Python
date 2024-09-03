22a9387f4815b1b4654315352177640f34c21a1c
AnthonyTheUnwise <ctw20.anthony.arundelfrtus@corketb.ie>
2024-09-03 13:44:59 +0100

    Add files via upload

---
+++ Intro to Python.py
@@ -0,0 +1,23 @@
+#Author: Anthony :D
+#Sept 2 2024
+#Testing / learning how to usethis properly
+print("Hello User!!!")
+print(9523.569245*3.062951)
+Name = input("Name please: ")
+print("Hai" , Name,"!")
+Rhetoric = input("Are you gonna click yes? (Y/N)")
+if Rhetoric == "Y" :
+    print("Congrats?!?")
+else:
+    print("I mean you still clicked...")
+
+#Time to loop
+
+Count = 0
+while Count<500:
+    print(Count)
+    Count+=1
+print("Ok, was worried that wouldnt end...")
+print("Hello \"World")
+#Code above is buggy
+    
