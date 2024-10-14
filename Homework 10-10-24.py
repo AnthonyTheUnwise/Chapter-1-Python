4721b25f63a28caabeabffc5f9c6cc805487deb4
AnthonyTheUnwise <ctw20.anthony.arundelfrtus@corketb.ie>
2024-10-14 09:35:01 +0100

    Add files via upload

---
+++ Homework 10-10-24.py
@@ -0,0 +1,29 @@
+"""
+Anthony ;D
+Coding hw
+"""
+
+"""
+An iteration is a version of a while loop ran in code
+A loop is a repeat of a statement and has many iterations
+Boolean
+True
+:
+"""
+
+#Task 1
+Counter = 1000
+Count = 0
+while Counter < 1501:
+    Count = Count + Counter
+    Counter = Counter + 1
+print(Count)
+
+#Task 2
+Num2 = 0
+Number = 0
+while Number < 6:
+    Num = int(input("enter a number:"))
+    Num2 = Num2 + Num
+    Number = Number + 1
+print(Num2)
