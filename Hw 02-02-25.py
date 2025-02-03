7085c164fcdfccc7360c89a6039def64e5c8921a
AnthonyTheUnwise <ctw20.anthony.arundelfrtus@corketb.ie>
2025-02-03 23:41:00 +0000

    Add files via upload

---
+++ Hw 02-02-25.py
@@ -0,0 +1,29 @@
+"""
+Anthony ;D
+02/02/25
+Tasks 6-8, pretty much it tho
+"""
+
+#Task 6
+
+def Homework():
+    print("The start!")
+    print("Jack loves to do his homework")
+    print("He never misses a day")
+    print("He even loves the men in white")
+    print("Who are taking him away")
+    print("The end!")
+Homework()
+
+#Task 7
+
+def Limerick():
+    print("Way down south where bananas grow,")
+    print("There's a grasshopper the size of an elephant's toe.")
+    print("The elephant said, with tears in his eyes,")
+    print("'Pick on somebody your own size.'")
+Limerick()
+
+# Task 8
+
+#Error is saying Display instead of Show, thats abt it
